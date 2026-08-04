049b2dd65131ab731dba9ac97b478e2f21184bab
yougq <youguoqing1@huawei.com>
2026-08-04 11:33:03 +0800

    修复扫描问题

--- common/example/agentcore-ext-deepagent-remote-a2a-demo/agent-a-deepagent-runtime/src/main/java/com/openjiuwen/example/agentcoreext/deepa/DeepAgentLlmProperties.java
+++ common/example/agentcore-ext-deepagent-remote-a2a-demo/agent-a-deepagent-runtime/src/main/java/com/openjiuwen/example/agentcoreext/deepa/DeepAgentLlmProperties.java
@@ -19,7 +19,7 @@ import java.util.Map;
 public class DeepAgentLlmProperties {
     private String provider = "OpenAI";
     private String apiKey = "";
-    private String apiBase = "https://api.deepseek.com";
+    private String apiBase = "";
     private String modelName = "deepseek-chat";
     private boolean isSslVerify = true;
     private String systemPrompt = """
